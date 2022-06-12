75d91d4f6e07d5f436db1264d3607514b15bfd2f
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2022-06-12 16:01:56 +0530

    resAPi

---
+++ restAPI/app.js
@@ -0,0 +1,32 @@
+let express = require('express');
+let app = express();
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
+const dotenv = require('dotenv');
+dotenv.config()
+let port = process.env.PORT ||8230;
+const mongoUrl = process.env.mongoUrl
+
+
+app.get('/',(req,res)=>{
+    res.send("welcome uviiiii")
+})
+
+app.get('/home',(req,res)=>{
+    db.collection('product').find().toArray((err,result)=>{
+        if (err) throw err;
+        res.send(result)
+
+    })
+})
+
+
+
+// Connection with db
+MongoClient.connect(mongoUrl, (err, client) => {
+    if (err) console.log(`Error while connecting`);
+    db = client.db('ishop');
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`)
+    })
+})
